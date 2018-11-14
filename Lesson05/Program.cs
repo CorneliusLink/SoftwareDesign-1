a5b57d2f69873e62cc7609ff9025f695d6d2d20d
Codeslave84 <38300498+Codeslave84@users.noreply.github.com>
2018-11-14 14:49:27 +0100

    .

--- Lesson05/Program.cs
+++ Lesson05/Program.cs
@@ -6,40 +6,11 @@ namespace Lesson05
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter Text!");
-            inputText(Console.ReadLine());            
+            Console.WriteLine("Enter Text");
+            string enteredText = Console.ReadLine();
         }
 
 
-        static String inputText(string enterdText)
-        {
-            Console.WriteLine("select the method");
-            Console.WriteLine("1 to turn the whole Text");
-            Console.WriteLine("2 to turn every two words");
-            
-            int select = Int32.Parse(Console.ReadLine());
-            
-            switch(select){
-                case 1:
-                    return turnWholeText(enterdText);
-                    break;
-                case 2:
-                    return turnTwoWords(enterdText);
-                    break;
-                default:
-                    return "";
-                    break;
-            }
-        }
-
-        static String turnWholeText(string enterdText)
-        {
-            return "hallo 1";
-        }
-
-        static String turnTwoWords(string enterdText)
-        {
-            return "hallo 2";
-        }
+        
     }
 }
