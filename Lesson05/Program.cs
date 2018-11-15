09f24db8cdc74034f5490cfa1dbe242f9be646d1
Codeslave84 <38300498+Codeslave84@users.noreply.github.com>
2018-11-15 15:16:35 +0100

    Diagramm überarbeitet

---
+++ Lesson05/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Lesson05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Gebe einen Text ein:");
+            var text = Console.ReadLine();
+            ReverseLetters(text);
+            ReverseWords(text);
+            ReverseSentence(text);
+        }
+
+        static string ReverseLetters(string text)
+        {
+            char [] letters = new char [text.Length];
+            char.Reverse;
+            return new String(char);
+        }
+
+        static string ReverseWords(string text)
+        {
+            string [] words = text.Split(' ');
+            words.Reverse;
+            text = String.Join(" ", words);
+
+            return text;
+        }
+
+        static string ReverseSentence(string text)
+        {
+            string [] sentence = text.Split(' ');
+            for(;;i--)
+            {
+                outputWords[i];
+                
+            }
+
+            sentence.Reverse;
+            return;
+
+        }
+    }
+}
