0d83653e0e37356c030814e79ae623b78d993dc4
Codeslave84 <38300498+Codeslave84@users.noreply.github.com>
2018-11-17 12:10:49 +0100

    .

--- Lesson05/Program.cs
+++ Lesson05/Program.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 
 namespace Lesson05
 {
@@ -6,24 +6,53 @@ namespace Lesson05
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Gebe einen Text ein:");
+            Console.WriteLine("Bitte einen Satz eingeben:");
             var text = Console.ReadLine();
-            
-            Console.WriteLine(ReverseLetters(text));
-            //Console.WriteLine(ReverseWords(text));
 
-        static string ReverseLetters (string text)
-        {
-            char [] letters = new char [text.Length];
-            string charString = "";
+            string senctence = ReverseSentence(text);
+            string output = ReverseWords(text);
+            string letters = ReverseLetters(text);
 
-            for (int i = text.Length-1, k=0; i>=0; i--, k++)
+            Console.WriteLine(senctence);
+            Console.WriteLine(output);
+            Console.WriteLine(letters);
+        }
+
+        static string ReverseLetters(string text){
+            string [] wordText = text.Split(' ');
+            string [] outputText = new string [wordText.Length];
+            string wordIsTurned = "";
+            string output = "";
+
+            for(int i = 0; i <= wordText.Length-1; i++)
             {
-                letters[k] = text[i];
-                charString += letters[k];
+                wordIsTurned = ReverseSentence(wordText[i]);
+                outputText[i] = wordIsTurned;
             }
+            
+            output = string.Join(" ", outputText);
 
-            return charString;
+            return output;
+        } 
+
+        static string ReverseSentence(string text)
+        {
+            char [] letters = text.ToCharArray();
+            
+            Array.Reverse(letters);
+            text = new String(letters);
+
+            return text;
+        }
+
+        static string ReverseWords(string text)
+        {
+            string[] words = text.Split(' ');
+            
+            Array.Reverse(words);
+            text = string.Join(" ", words);
+
+            return text;
         }
     }
 }
---
+++ Test/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Text!");
+            string text = Console.ReadLine();
+            int length = arrayLength(text);
+            Console.WriteLine(TurnWordPare(text, length));
+        }
+
+        static int arrayLength(string text)
+        {
+            string [] words = text.Split(' ');
+            int length;
+
+            if (words.Length % 2 == 0)
+            {
+                length = words.Length;
+            }
+            else
+            {
+                length = words.Length+1;
+            }
+
+            return length;
+        }
+
+        static string TurnWordPare(string text, int length){
+            string outputText = ""; 
+            string [] words = text.Split(' ');
+            string [] newWord = new string [length];
+
+            for(int i = 0; i <= words.Length-1; i++){
+                if(i % 2 == 0)
+                {
+                    newWord[i+1] = words[i];
+                }
+                else
+                {
+                    newWord[i-1] = words[i];
+                }
+            }
+
+            outputText = string.Join(" ", newWord);
+
+            return outputText;
+        }
+    }
+}
