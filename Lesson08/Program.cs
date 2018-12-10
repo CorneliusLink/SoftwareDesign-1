a4bf44c68243ed5016d625f718aca55fa87c0c67
Cornelius Link <44768123+CorneliusLink@users.noreply.github.com>
2018-12-10 16:02:51 +0100

    Update Program.cs

--- Lesson08/Program.cs
+++ Lesson08/Program.cs
@@ -7,7 +7,7 @@ namespace Lesson07
         static int score;
         private List<Quizelement> questionPool;
 
-        void GenerateDefaultQuesions(){
+        void GenerateDefaultQuesions(){ //Die Methode hat keinen Inhalt
             
         }
 
@@ -30,7 +30,7 @@ namespace Lesson07
             }
         }
 
-        void PlayTheGame(){
+        void PlayTheGame(){ //Der Methodenname ist evtl. etwas zu unspezifisch
             Quizelement.ShowQuestion();
 
             Console.WriteLine("Enter the Answer");
@@ -49,13 +49,13 @@ namespace Lesson07
                 
         }
 
-        GetUserInteraction(){
+        GetUserInteraction(){ //Auch hier ist der Methodenname noch etwas zu unklar, bzw. die Frage ist eher, was die Funktion hier genau macht, falls sie die nachfolgende Methode nicht wrappen soll -> kein Inhalt
 
         }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Player, please choose/np for play, a to add a new quiz, q to quit the game");
-            string choise = Console.ReadLine();
+            string choise = Console.ReadLine(); //Hier ist vermutlich "choice" gemeint. "choise" ist laut Google zwar eine alternative Schreibweise für "choice" aber weder im Oxford, noch im Merriam-Webster Dictionary gibt es einen Eintrag dafür. Ich würde also eher sagen, dass man hier die korrekte Schreibweise verwendet, um beim Review niemanden zu verwirren.
             
             switch(choise){
                 case "p":
@@ -67,6 +67,7 @@ namespace Lesson07
                 case "q":
                     Environment.Exit(-1);
                     break;
+                //switch cases sollten m.W. immer einen default case haben, falls es z.B. zu unerwarteten Eingaben kommt.
             }
 
             Console.WriteLine("Thanks for playing" + score);
